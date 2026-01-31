1b95ca056371e7fd903c47f0c295dd1160551a8a
Markus Chu <markuschu06@gmail.com>
2026-01-31 11:18:35 -0800

    almost done issue 15

--- src/tests/differential_test.py
+++ src/tests/differential_test.py
@@ -72,53 +72,67 @@ def test_div(shape: Tuple[int, ...]) -> None:
     tester = OperationTest(torch.div, lambda a, b: a / b, (1e-10, 10))
     tester.run_test(shape)
 
-
 # compound test
 @pytest.mark.parametrize("shape", [(3, 3), (2, 4)])
 def test_compound(shape: Tuple[int, ...]) -> None:
-    # torchTensors = []
-    # ourTensors = []
-    # for i in range(N): # init the tensors
-
+    # Start with two random tensors
     a_torch, a = random_tensors(shape)
     b_torch, b = random_tensors(shape)
-
-    # example expression: a*b + a/b
-    # res_torch = a_torch * b_torch + a_torch / b_torch
-    # res_our = a * b + a / b
+    
+    # Keep track of all results in a list (the "pool")
+    torch_pool = [a_torch, b_torch]
+    our_pool = [a, b]
 
     for n in range(0, 10):
-        # pick random number from 1-9
+        # Pick two random items from the pool
+        i1 = np.random.randint(0, len(torch_pool))
+        i2 = np.random.randint(0, len(torch_pool))
+        
+        t1, t2 = torch_pool[i1], torch_pool[i2]
+        v1, v2 = our_pool[i1], our_pool[i2]
+
         i = np.random.randint(1, 10)
+        res_torch, res_our = None, None 
+
         match i:
             case 1:  # "+" operator
-                res_torch = a_torch + b_torch
-                res_our = a + b
+                res_torch = t1 + t2
+                res_our = v1 + v2
             case 2:  # "*" operator
-                res_torch = a_torch * b_torch
-                res_our = a * b
+                res_torch = t1 * t2
+                res_our = v1 * v2
             case 3:  # "-" operator
-                a_torch - b_torch
-                res_our = a - b
+                res_torch = t1 - t2
+                res_our = v1 - v2
             case 4:  # "/" operator
-                a_torch / b_torch
-                res_our = a / b
-            case 5:  # "exp" operator(uniary)
-                res_torch = a_torch.exp()
-                res_our = a.exp()
-            case 7:  # "-"   operator(uniary)
-                res_torch = a_torch * -1
-                res_our = a * -1
-            case 8:  # "@"   operator
-                res_torch = a_torch @ b_torch
-                res_our = a @ b
-            case 9:  # "**"  operator
-                res_torch = a_torch**3
-                res_our = a**3
+                res_torch = t1 / (t2 + 0.0000000000000001) # avoid 0
+                res_our = v1 / (v2 + 0.0000000000000001)
+            case 5:  # "exp" operator
+                res_torch = t1.exp()
+                res_our = v1.exp()
+            case 7:  # unary "-" operator
+                res_torch = t1 * -1
+                res_our = v1 * -1
+            case 8:  # "@" operator
+                if t1.shape == t2.shape and len(t1.shape) == 2:
+                    res_torch = t1 @ t2
+                    res_our = v1 @ v2
+            case 9:  # "**" operator
+                res_torch = t1**3
+                res_our = v1**3
+
+        # add result to pool so it can be used again
+        if res_torch is not None:
+            torch_pool.append(res_torch)
+            our_pool.append(res_our)
+    
+    # compare final result
+    final_torch = torch_pool[-1]
+    final_our = our_pool[-1]
 
     our_data = (
-        res_our.data if isinstance(res_our.data, np.ndarray) else np.array(res_our.data)
+        final_our.data if isinstance(final_our.data, np.ndarray) else np.array(final_our.data)
     )
     assert np.allclose(
-        our_data, res_torch.numpy(), rtol=1e-5, atol=1e-8
-    )  # nessacry when doing compound tests?
+        our_data, final_torch.numpy(), rtol=1e-5, atol=1e-8
+    )
